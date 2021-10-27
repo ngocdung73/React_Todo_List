84d101200ee04543d13db5148a7dd18bd701578a
Ngọc Dũng <ngocdungsss81@gmail.com>
2021-10-27 20:08:22 +0700

    Todo_List

--- src/redux/reducers/todolist.reducers.js
+++ src/redux/reducers/todolist.reducers.js
@@ -16,7 +16,8 @@ const todoListReducer = createReducer(initialState, {
   [TODO_LIST_ACTIONS.TODO_DELETE]: (state, action) => {
     const {id} = action.payload
     const newTasksList = [...state.tasksList]
-    newTasksList.splice(id, 1)
+    const indexToDo = newTasksList.findIndex(item => item.id === id)
+    newTasksList.splice(indexToDo, 1)
     return {
       ...state,
       tasksList: newTasksList,
@@ -26,7 +27,8 @@ const todoListReducer = createReducer(initialState, {
   [TODO_LIST_ACTIONS.TODO_EDIT]: (state, action) => {
     const {id} = action.payload
     const newTasksList = [...state.tasksList]
-    newTasksList.splice(id, 1, action.payload)
+    const indexToDo = state.tasksList.findIndex(item => item.id === id)
+    newTasksList.splice(indexToDo, 1, action.payload)
     return {
       ...state,
       tasksList: newTasksList,
